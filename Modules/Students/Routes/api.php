a001f8ff72e84ebc52929101139e906dc34d1887
yumiazusa <yumiazusa@hotmail.com>
2023-03-14 16:27:34 +0800

    test1

--- Modules/Students/Routes/api.php
+++ Modules/Students/Routes/api.php
@@ -3,7 +3,7 @@
  * @Author: yumiazusa yumiazusa@hotmail.com
  * @Date: 2023-02-28 13:32:20
  * @LastEditors: yumiazusa yumiazusa@hotmail.com
- * @LastEditTime: 2023-03-07 15:24:36
+ * @LastEditTime: 2023-03-14 16:27:23
  * @FilePath: /www/miledo/server/Modules/Students/Routes/api.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,14 +25,14 @@ Route::group(["prefix"=>"v1/students","middleware"=>"StudentsApiAuth"],function 
     // 会员列表
     Route::get('students/index', 'v1\StudentsController@index');
       //添加
-      Route::post('students/store', 'v1\studentsController@store');
+      Route::post('students/store', 'v1\StudentsController@store');
       //编辑页面
-      Route::get('students/edit', 'v1\studentsController@edit');
+      Route::get('students/edit', 'v1\StudentsController@edit');
       //编辑提交
-      Route::put('students/update', 'v1\studentsController@update');
+      Route::put('students/update', 'v1\StudentsController@update');
       //调整状态
-      Route::put('students/status', 'v1\studentsController@status');
+      Route::put('students/status', 'v1\StudentsController@status');
       //初始化密码
-      Route::put('students/updatePwd', 'v1\studentsController@updatePwd');
+      Route::put('students/updatePwd', 'v1\StudentsController@updatePwd');
 
 });
